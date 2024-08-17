8c0b385f1e84f618fabb131f7f87a8ef898af210
MAHIM-JU-CSE <mahbubislammahim@gmail.com>
2024-08-17 17:08:36 +0600

    Projects imp

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -1,52 +1,62 @@
-import React, { useState } from 'react';
-import { AiFillEye, AiFillGithub } from 'react-icons/ai';
-import { motion } from 'framer-motion';
+import React, { useState } from "react";
+import { AiFillEye, AiFillGithub } from "react-icons/ai";
+import { motion } from "framer-motion";
 
-import { AppWrap, MotionWrap } from '../../wrapper';
-import './Work.scss';
+import { AppWrap, MotionWrap } from "../../wrapper";
+import "./Work.scss";
 
 // Import the static image from the assets folder
-import about0q from '../../assets/about01.png';
+
+import biofloc from "../../assets/biofloc.PNG";
+import objectdetection from "../../assets/objectdetection.png";
+import portfolio from "../../assets/portfolio.png";
+import ai from "../../assets/ai.png";
 
 // Hardcoded portfolio data with the same image
 const works = [
   {
-    title: 'UI/UX Design Project',
-    description: 'A project focused on UI/UX design principles and practices.',
-    imgUrl: about0q,
-    projectLink: 'https://example.com',
-    codeLink: 'https://github.com/example',
-    tags: ['UI/UX'],
+    title: "IoT Project",
+    description:
+      "This is an IoT-based water quality (pH, temperature, TDS, ammonia, Dissolved Oxygen) monitoring system for biofloc. Machine learning is also applied to take decision about the water quality.",
+    imgUrl: biofloc,
+    projectLink:
+      "https://github.com/MAHIM-JU-CSE/Iot-based-biofloc-water-quality-monitoring-",
+    codeLink:
+      "https://github.com/MAHIM-JU-CSE/Iot-based-biofloc-water-quality-monitoring-",
+    tags: ["IoT"],
   },
   {
-    title: 'Web App Development',
-    description: 'A project focused on UI/UX design principles and practices.',
-    imgUrl: about0q,
-    projectLink: 'https://example.com',
-    codeLink: 'https://github.com/example',
-    tags: ['Web App'],
+    title: "Object Detection",
+    description:
+      "This is an object detection project using OpenCV, Tensorflow and cvlib. Underneath it uses YOLOv4 model trained on COCO dataset capable of detecting 80 common objects in context.",
+    imgUrl: objectdetection,
+    projectLink: "https://github.com/MAHIM-JU-CSE/Object_detection",
+    codeLink: "https://github.com/MAHIM-JU-CSE/Object_detection",
+    tags: ["Machine Learning"],
   },
   {
-    title: 'Mobile App Design',
-    description: 'A project focused on UI/UX design principles and practices.',
-    imgUrl: about0q,
-    projectLink: 'https://example.com',
-    codeLink: 'https://github.com/example',
-    tags: ['Mobile App'],
+    title: "Web App",
+    description:
+      "This is a web project using Reactjs, Framer motion, SCSS, and Emailjs. The site is designed fully responsive using media query. It is made to represent my academic and professional details",
+    imgUrl: portfolio,
+    projectLink: "https://mahim-ju-cse.github.io/portfolio/",
+    codeLink: "https://github.com/MAHIM-JU-CSE/portfolio",
+    tags: ["Web App"],
   },
   {
-    title: 'React JS Project',
-    description: 'Building a project with React JS for interactive web interfaces.',
-    imgUrl: about0q,
-    projectLink: 'https://example.com',
-    codeLink: 'https://github.com/example',
-    tags: ['React JS'],
+    title: "Generative AI",
+    description:
+      "This is a Conversational AI project leveraging the LLaMA 3.1 8B and Mistral 7B models, integrated through Ollama and OpenWebUI for robust and responsive natural language interactions.",
+    imgUrl: ai,
+    projectLink: "",
+    codeLink: "",
+    tags: ["Generative AI"],
   },
 ];
 
 const Work = () => {
   const [filterWork, setFilterWork] = useState(works);
-  const [activeFilter, setActiveFilter] = useState('All');
+  const [activeFilter, setActiveFilter] = useState("All");
   const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
 
   const handleWorkFilter = (item) => {
@@ -56,7 +66,7 @@ const Work = () => {
     setTimeout(() => {
       setAnimateCard({ y: 0, opacity: 1 });
 
-      if (item === 'All') {
+      if (item === "All") {
         setFilterWork(works);
       } else {
         setFilterWork(works.filter((work) => work.tags.includes(item)));
@@ -66,18 +76,24 @@ const Work = () => {
 
   return (
     <>
-      <h2 className="head-text">My Innovative <span>Projects</span></h2>
+      <h2 className="head-text">
+        Personal Innovative <span>Projects</span>
+      </h2>
 
       <div className="app__work-filter">
-        {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
-          <div
-            key={index}
-            onClick={() => handleWorkFilter(item)}
-            className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
-          >
-            {item}
-          </div>
-        ))}
+        {["IoT", "Machine Learning", "Mobile App", "React JS", "All"].map(
+          (item, index) => (
+            <div
+              key={index}
+              onClick={() => handleWorkFilter(item)}
+              className={`app__work-filter-item app__flex p-text ${
+                activeFilter === item ? "item-active" : ""
+              }`}
+            >
+              {item}
+            </div>
+          )
+        )}
       </div>
 
       <motion.div
@@ -92,13 +108,17 @@ const Work = () => {
 
               <motion.div
                 whileHover={{ opacity: [0, 1] }}
-                transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
+                transition={{
+                  duration: 0.25,
+                  ease: "easeInOut",
+                  staggerChildren: 0.5,
+                }}
                 className="app__work-hover app__flex"
               >
                 <a href={work.projectLink} target="_blank" rel="noreferrer">
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.90] }}
+                    whileHover={{ scale: [1, 0.9] }}
                     transition={{ duration: 0.25 }}
                     className="app__flex"
                   >
@@ -108,7 +128,7 @@ const Work = () => {
                 <a href={work.codeLink} target="_blank" rel="noreferrer">
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.90] }}
+                    whileHover={{ scale: [1, 0.9] }}
                     transition={{ duration: 0.25 }}
                     className="app__flex"
                   >
@@ -120,7 +140,9 @@ const Work = () => {
 
             <div className="app__work-content app__flex">
               <h4 className="bold-text">{work.title}</h4>
-              <p className="p-text" style={{ marginTop: 10 }}>{work.description}</p>
+              <p className="p-text" style={{ marginTop: 10 }}>
+                {work.description}
+              </p>
 
               <div className="app__work-tag app__flex">
                 <p className="p-text">{work.tags[0]}</p>
@@ -134,7 +156,7 @@ const Work = () => {
 };
 
 export default AppWrap(
-  MotionWrap(Work, 'app__works'),
-  'projects',
-  'app__primarybg',
+  MotionWrap(Work, "app__works"),
+  "projects",
+  "app__primarybg"
 );
